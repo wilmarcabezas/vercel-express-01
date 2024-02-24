45be1560be103caedc1946292afc750f8f532e86
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-02-24 11:19:40 -0500

    Modifico Controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ message: 'Product List'});
+    res.json({ message: 'Product List.'});
 });
 
 
